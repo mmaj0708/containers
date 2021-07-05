fd94bb5caf92146bd53d6412edc19321b84a8f60
Melchior MAJ <mmaj@e1r11p21.42.fr>
2021-07-05 19:31:18 +0200

    CONTAIIIIINEEEER

--- main.cpp
+++ main.cpp
@@ -3,31 +3,49 @@
 #include <iostream>
 #include "vector.hpp"
 
+
 int main()
 {
-	// int i = 0;
-	std::vector<int> tab(5, 10);
-	std::vector<int>::iterator it = tab.begin();
-	std::cout << tab.size() << std::endl;
-	std::cout << tab.max_size() << std::endl;
-	std::cout << tab.capacity() << std::endl;
-	std::cout << tab.empty() << std::endl;
+	std::cout << "***VECTOR TEST***\n" << std::endl;
+	std::cout << "**DEFAULT CONSTRUCTOR TEST**\n" << std::endl;
+	std::cout << "*STD::VECTOR*" << std::endl;
+	std::vector<int> tab;
+	std::cout << "size : " << tab.size() << std::endl;
+	std::cout << "max_size : " << tab.max_size() << std::endl;
+	std::cout << "capacity : " << tab.capacity() << std::endl;
+	std::cout << "empty : " << tab.empty() << std::endl;
 
-	std::cout << "bonjour " << tab[0] << std::endl;
-	while (it != tab.end())
-	{
-		std::cout << *it << std::endl;
-		it++;
-	}
+	std::cout << "\n*FT::VECTOR*" << std::endl;
+	ft::vector<int>	ft_tab;
+	std::cout << "size : " << ft_tab.size() << std::endl;
+	std::cout << "max_size : " << ft_tab.max_size() << std::endl;
+	std::cout << "capacity : " << ft_tab.capacity() << std::endl;
+	std::cout << "empty : " << ft_tab.empty() << std::endl;
+
+	std::cout << "\n**FILL CONSTRUCTOR TEST**\n" << std::endl;
+	std::cout << "*STD::VECTOR*" << std::endl;
+	std::vector<int> tab2(5, 10);
+	std::cout << "size : " << tab2.size() << std::endl;
+	std::cout << "max_size : " << tab2.max_size() << std::endl;
+	std::cout << "capacity : " << tab2.capacity() << std::endl;
+	std::cout << "empty : " << tab2.empty() << std::endl;
+	std::cout << tab2[0] << std::endl;
+	std::cout << tab2[1] << std::endl;
+	std::cout << tab2[2] << std::endl;
+	std::cout << tab2[3] << std::endl;
+	std::cout << tab2[4] << std::endl;
+
+	std::cout << "\n*FT::VECTOR*" << std::endl;
+	ft::vector<int>	ft_tab2(5, 10);
+	std::cout << "size : " << ft_tab2.size() << std::endl;
+	std::cout << "max_size : " << ft_tab2.max_size() << std::endl;
+	std::cout << "capacity : " << ft_tab2.capacity() << std::endl;
+	std::cout << "empty : " << ft_tab2.empty() << std::endl;
+	std::cout << ft_tab2[0] << std::endl;
+	std::cout << ft_tab2[1] << std::endl;
+	std::cout << ft_tab2[2] << std::endl;
+	std::cout << ft_tab2[3] << std::endl;
+	std::cout << ft_tab2[4] << std::endl;
+
 	
-	// std::cout << "\nMY VECTOR" << std::endl;
-	// ft::vector<int>	ft_tab(5, 10);
-
-	// std::cout << ft_tab.size() << std::endl;
-	// std::cout << ft_tab[0] << std::endl;
-	// std::cout << ft_tab[1] << std::endl;
-	// std::cout << ft_tab[2] << std::endl;
-	// std::cout << ft_tab[3] << std::endl;
-	// std::cout << ft_tab[4] << std::endl;
-	// std::cout << ft_tab[5] << std::endl;
 }
--- vector.hpp
+++ vector.hpp
@@ -16,79 +16,121 @@ namespace	ft	{
 
 	public:
 
-    typedef T                                        value_type;
-    typedef Allocator                                allocator_type;
-    typedef typename allocator_type::size_type       size_type;
+    typedef T                                        	value_type;
+    typedef Allocator                                	allocator_type;
+    typedef typename allocator_type::size_type       	size_type;
+    typedef typename allocator_type::reference       	reference;
+    typedef typename allocator_type::const_reference 	const_reference;
+    // typedef implementation-defined                   	iterator;
+    // typedef implementation-defined                   	const_iterator;
+    typedef typename allocator_type::difference_type 	difference_type;
+    typedef typename allocator_type::pointer         	pointer;
+    typedef typename allocator_type::const_pointer   	const_pointer;
+    // typedef std::reverse_iterator<iterator>          	reverse_iterator;
+    // typedef std::reverse_iterator<const_iterator>    	const_reverse_iterator;
 
+	/*	CONSTRUCTOR	*/
 	/* default constructor */
-	explicit vector (const allocator_type& alloc = allocator_type()) : _size(0), _alloc(alloc) {}
+	explicit vector (const allocator_type& alloc = allocator_type())
+	: _size(0), _max_size(alloc.max_size()), _capacity(0), _alloc(alloc) {}
 
 	/* fill constructor */
-	// explicit vector (size_type n, const value_type& val = value_type(),
-	// const allocator_type& alloc = allocator_type()) : _size(n), _alloc(alloc)
-	// {
-	// 	size_type	i = 0;
+	explicit vector (size_type n, const value_type& val = value_type(),
+	const allocator_type& alloc = allocator_type())
+	: _size(n), _max_size(alloc.max_size()), _capacity(n), _alloc(alloc) {
+		
+		size_type	i = 0;
 
-	// 	_tab = _alloc.allocate(n);
-	// 	while (i < n)
-	// 	{
-	// 		_tab[i] = val;
-	// 		i++;
-	// 	}
-	// }
-
-	/* range constructor */	
-	// template <class InputIterator>
-    // vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type())
-	// : _alloc(alloc)
-	// {
-	// 	InputIterator	i = first;
-	// 	value_type		j = 0;
-
-	// 	_tab = new value_type[last - first];
-	// 	while (i != last)
-	// 	{
-	// 		_tab[j] = *i;
-	// 		i++;
-	// 	}
-	// }
-
-void	push_back (const value_type& val);
-
-
-	value_type	& operator[]( value_type i )
-	{
-		return (_tab[i]);
+		_tab = _alloc.allocate(n);
+		while (i < n)
+		{
+			_alloc.construct(&_tab[i], val);
+			i++;
+		}
 	}
 
-	int		size() {return _size;}
-	
-	// PROCHAINE FOIS CONSTRUCTEUR SUIVANT !!
-
-	~vector() {}
-    // typedef typename allocator_type::reference       reference;
-    // typedef typename allocator_type::const_reference const_reference;
-    // typedef implementation-defined                   iterator;
-    // typedef implementation-defined                   const_iterator;
-    // typedef typename allocator_type::difference_type difference_type;
-    // typedef typename allocator_type::pointer         pointer;
-    // typedef typename allocator_type::const_pointer   const_pointer;
-    // typedef std::reverse_iterator<iterator>          reverse_iterator;
-    // typedef std::reverse_iterator<const_iterator>    const_reverse_iterator;
-		
-	// explicit vector (size_type n, const value_type& val = value_type(),
-	//                  const allocator_type& alloc = allocator_type());
-
+	// /* range constructor */	
 	// template <class InputIterator>
-	// vector (InputIterator first, InputIterator last,
-	//                  const allocator_type& alloc = allocator_type());
-	// vector (const vector& x);
+    // vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()) {}
+
+	// /* copy constructor */
+	// vector (const vector& x) {}
+
+	/* destructor */
+	~vector() {
+		size_type	i = 0;
+
+		while (i < _size)
+		{
+			_alloc.destroy(&_tab[i]);
+			i++;
+		}
+		_alloc.deallocate(_tab, _size);
+	}
+
+	// /* assignation */
+	// vector& operator= (const vector& x);
+
+	// /* ITERATOR */
+	// iterator				begin();
+	// const_iterator			begin() const;
+	// iterator				end();
+	// const_iterator			end() const;
+	// reverse_iterator		rbegin();
+	// const_reverse_iterator	rbegin() const;
+	// reverse_iterator		rend();
+	// const_reverse_iterator	rend() const;
+
+	// /* CAPACITY */
+	size_type	size() const { return _size; } // return number of elmt in the vector
+	size_type	max_size() const { return _max_size; }
+	// void		resize (size_type n, value_type val = value_type());
+	size_type	capacity() const { return _capacity; }	// return size of allocated storage
+	bool		empty() const { if (_size > 0) return 0; return 1; }
+	// void		reserve (size_type n);
 	
+	// /* ELEMENT ACCESS */
+	value_type			&operator[]( value_type i ) { return (_tab[i]); }
+	
+	// reference			operator[] (size_type n);
+	// const_reference 	operator[] (size_type n) const;
+	
+	// reference			at (size_type n);
+	// const_reference 	at (size_type n) const;
+	
+	// reference			front();
+	// const_reference		front() const;	
+	
+	// reference			back();
+	// const_reference		back() const;
+	
+	// /* MODIFIERS */
+	// template <class InputIterator>
+	// void		assign (InputIterator first, InputIterator last);
+	// void		assign (size_type n, const value_type& val);
+
+	// void		push_back (const value_type& val) {}
+	// void		pop_back();
+	
+	// iterator	insert (iterator position, const value_type& val);
+	// void		insert (iterator position, size_type n, const value_type& val);
+	// template <class InputIterator>
+    // void		insert (iterator position, InputIterator first, InputIterator last);
+
+	// iterator	erase (iterator position);
+	// iterator	erase (iterator first, iterator last);
+
+	// void 		swap (vector& x);
+
+	// void clear();
+
 	private:
 
-	int			_size;
-	Allocator	_alloc;
-	value_type	*_tab;
+	size_type		_size;
+	size_type		_max_size;
+	size_type		_capacity;
+	allocator_type	_alloc;
+	value_type		*_tab;
 
 	};
 }
