0de054d72e17eb5e3ff2b984d6a2b423bbbe3c0e
Melchior MAJ <mmaj@e1r11p14.42.fr>
2021-08-20 15:09:38 +0200

    erase done

--- containers_test/srcs/vector/erase.cpp
+++ containers_test/srcs/vector/erase.cpp
@@ -40,6 +40,7 @@ void	checkErase(TESTED_NAMESPACE::vector<TESTED_TYPE> const &vct,
 					TESTED_NAMESPACE::vector<TESTED_TYPE>::const_iterator const &it)
 {
 	std::cout << *it << std::endl;
+	// std::cout << *(vct.end() - 1) << std::endl;
 	static int i = 0;
 	std::cout << "[" << i++ << "] " << "erase: " << it - vct.begin() << std::endl;
 	printSize(vct);
@@ -56,22 +57,23 @@ int		main(void)
 	checkErase(vct, vct.erase(vct.begin() + 2));
 
 	checkErase(vct, vct.erase(vct.begin()));
-	checkErase(vct, vct.erase(vct.end() - 1)); // PROBLEME ICI 
+	// std::cout << *(vct.end() - 1) << std::endl;
+	checkErase(vct, vct.erase(vct.end() - 1));
 
-	// checkErase(vct, vct.erase(vct.begin(), vct.begin() + 3));
-	// checkErase(vct, vct.erase(vct.end() - 3, vct.end() - 1));
+	checkErase(vct, vct.erase(vct.begin(), vct.begin() + 3));
+	checkErase(vct, vct.erase(vct.end() - 3, vct.end() - 1));
 
-	// vct.push_back("Hello");
-	// vct.push_back("Hi there");
-	// printSize(vct);
-	// checkErase(vct, vct.erase(vct.end() - 3, vct.end()));
+	vct.push_back("Hello");
+	vct.push_back("Hi there");
+	printSize(vct);
+	checkErase(vct, vct.erase(vct.end() - 3, vct.end()));
 
-	// vct.push_back("ONE");
-	// vct.push_back("TWO");
-	// vct.push_back("THREE");
-	// vct.push_back("FOUR");
-	// printSize(vct);
-	// checkErase(vct, vct.erase(vct.begin(), vct.end()));
+	vct.push_back("ONE");
+	vct.push_back("TWO");
+	vct.push_back("THREE");
+	vct.push_back("FOUR");
+	printSize(vct);
+	checkErase(vct, vct.erase(vct.begin(), vct.end()));
 
 	return (0);
 }
